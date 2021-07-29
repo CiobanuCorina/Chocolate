2c1b1ea323fe3a2c033711b5999631a48d84ca2c
Corina CIOBANU <cociobanu@pentalog.com>
2021-07-29 15:20:02 +0300

    using Storage constant

--- product/product.js
+++ product/product.js
@@ -5,7 +5,7 @@ const constants = {
 
 const Utils = {
     formatPrice(price) {
-        return price.toLocaleString("en-US", {
+        return parseFloat(price).toLocaleString("en-US", {
             style: "currency",
             currency: "USD"
         });
@@ -26,6 +26,12 @@ const Storage = {
     },
     removeItem(key) {
         window.localStorage.removeItem(key);
+    },
+    isEmpty(key) {
+        return window.localStorage.getItem(key) === null;
+    },
+    isEmptyArray(key) {
+        return window.localStorage.getItem(key) === '[]';
     }
 }
 let form = document.querySelector('form');
@@ -33,9 +39,14 @@ let form = document.querySelector('form');
 if (form) {
     form.addEventListener('submit', (e) => {
         const formData = new FormData(e.target);
-        let product = new Product(formData.get('name'), formData.get('price'),
-            formData.get('image'), formData.get('description'), formData.get('amount'));
-        localStorage.setItem(product.name, JSON.stringify(product));
+        let products;
+        if(Storage.isEmpty(constants.PRODUCT_STORAGE_KEY)) {
+            products = [];
+        }
+        else {products = Storage.getItem(constants.PRODUCT_STORAGE_KEY);}
+        products.push(new Product(formData.get('name'), formData.get('price'),
+            formData.get('image'), formData.get('description'), formData.get('amount')));
+        Storage.setItem(constants.PRODUCT_STORAGE_KEY, products);
     });
 }
 
@@ -48,18 +59,21 @@ class Product {
         this.amount = amount;
     }
 
+    static findProduct(name) {
+        let products = Storage.getItem(constants.PRODUCT_STORAGE_KEY);
+        return products.find(product => product.name === name);
+    }
+
     static read() {
-        let items = {...localStorage};
+        let items = Storage.getItem( constants.PRODUCT_STORAGE_KEY);
         let product = '';
-        for (let item in items) {
-            if (item === 'cart') continue;
-            item = JSON.parse(localStorage.getItem(item));
+        items.forEach((item) => {
             product += `<div class="card" style="width: 18rem;">
                             <img src="images/${item.image}" alt="chocolate" class="img-responsive product img-fluid">
                             <div class="card-body">
                                 <p class="title">${item.name}</p>
                                 <p class="description">${item.description}</p>
-                                <p class="price">${item.price} $</p>
+                                <p class="price">${Utils.formatPrice(item.price)}</p>
                                 <div class="row" class="buttons">
                                     <div class="col-6">
                                         <button type="button" class="btn btn-custom cart" name="${item.name}">Add to cart</button><br><br>
@@ -71,24 +85,26 @@ class Product {
                                 </div>
                             </div>
                         </div>`;
-        }
+        })
+
         document.getElementById('products').innerHTML = product;
         const updateButtons = document.querySelectorAll('.update');
         updateButtons.forEach((element) => {
             element.addEventListener('click', () => {
-                let product = JSON.parse(localStorage.getItem(element.name));
+                let product = this.findProduct(element.name);
                 document.getElementById('name').value = product.name;
                 document.getElementById('price').value = product.price;
                 document.getElementById('description').value = product.description;
                 document.getElementById('image').value = product.image;
                 document.getElementById('amount').value = product.amount;
                 document.getElementById('update').addEventListener('click', () => {
-                    product.name = document.getElementById('name').value;
                     product.price = document.getElementById('price').value;
                     product.description = document.getElementById('description').value;
                     product.image = document.getElementById('image').value;
                     product.amount = document.getElementById('amount').value;
-                    localStorage.setItem(product.name, JSON.stringify(product));
+                    let products = Storage.getItem(constants.PRODUCT_STORAGE_KEY);
+                    products[products.findIndex(element => element.name === product.name)] = product;
+                    Storage.setItem(constants.PRODUCT_STORAGE_KEY, products);
                     location.reload();
                 })
             })
@@ -96,19 +112,21 @@ class Product {
         const addToCart = document.querySelectorAll('.cart');
         addToCart.forEach((element) => {
             element.addEventListener('click', () => {
-                let jsonProduct = JSON.parse(localStorage.getItem(element.name));
+                let jsonProduct = this.findProduct(element.name);
                 let product = new Product(jsonProduct.name, jsonProduct.price, jsonProduct.image, jsonProduct.description, jsonProduct.amount);
                 let cart = new Cart();
                 cart.addItem(product);
 
-                localStorage.setItem('cart', JSON.stringify(cart));
+                Storage.setItem(constants.CART_STORAGE_KEY, cart);
             })
         })
 
     }
 
     static delete(name) {
-        localStorage.removeItem(name);
+        let items = Storage.getItem(constants.PRODUCT_STORAGE_KEY);
+        items = items.filter((p) => p.name !== name);
+        Storage.setItem(constants.PRODUCT_STORAGE_KEY, items);
         location.reload();
     }
 }
@@ -119,13 +137,13 @@ class Cart {
     }
 
     load() {
-        this.items = (JSON.parse(localStorage.getItem('cart'))?.items ?? []).map(
+        this.items = (Storage.getItem(constants.CART_STORAGE_KEY)?.items ?? []).map(
             (productData) => new Product(productData.name, productData.price, productData.image, productData.description, productData.amount)
         );
     }
 
     save() {
-        localStorage.setItem('cart', JSON.stringify(this.items));
+        Storage.setItem(constants.CART_STORAGE_KEY, this.items);
     }
 
     addItem(product) {
@@ -185,12 +203,14 @@ class Cart {
 
 Product.read();
 
-document.querySelector('.delete').addEventListener('click', function () {
-    Product.delete(this.name);
+document.querySelectorAll('.delete').forEach(element => {
+    element.addEventListener('click', function() {
+        Product.delete(this.name);
+    })
 });
 document.getElementById('cart').addEventListener('click', function () {
-    if (localStorage.getItem('cart') === null ||
-        localStorage.getItem('cart').length === 2) {
+    if (Storage.isEmpty(constants.CART_STORAGE_KEY) ||
+        Storage.isEmptyArray(constants.CART_STORAGE_KEY)) {
         document.getElementById('cartBody').innerHTML = '<p>Your cart is empty</p>'
     } else {
         let cart = new Cart();
